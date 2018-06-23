c31cb4cbbc450b0075d91024df98a32994efe12e
Rob Brander <rob.brander@gmail.com>
2018-06-23 17:00:53 -0400

    add server polish and start to structure lib

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
 // maze-gen-server index.js
 
+const compression = require('compression');
 const express = require('express');
-const app = express();
+const url = require('url');
 const mazeGen = require('../lib/maze-gen');
 
-// const mazeGen = require('./maze-gen');
+const app = express();
+app.use(compression());
 
+// Routing constants
 const API_BASE = '/api/v1';
 const GENERATE_MAZE_BASE_ROUTE = `${API_BASE}/generateMaze`;
 const GENERATE_MAZE_WITH_DIMENSIONS_ROUTE = `${GENERATE_MAZE_BASE_ROUTE}/:width/:height`;
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Route
 app.get('/', (req, res) =>
   res
     .status(200)
     .sendFile(`${__dirname}/index.html`)
 );
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Route
 app.get(GENERATE_MAZE_WITH_DIMENSIONS_ROUTE, (req, res) => {
   console.log(`generateMaze() - ${req.url}`);
   const { width, height } = req.params;
@@ -29,18 +36,32 @@ app.get(GENERATE_MAZE_WITH_DIMENSIONS_ROUTE, (req, res) => {
   const iWidth = ~~Number(width);
   const iHeight = ~~Number(height);
   const mazeData = mazeGen.generateMaze(iWidth, iHeight);
+
+  // Check the query string for pretty=true
+  const query = url.parse(req.url, true).query;
+  const isPretty = query.pretty === 'true';
+  const result = isPretty ? JSON.stringify(mazeData, undefined, 2) : JSON.stringify(mazeData);
+  res.setHeader('Content-Type', 'application/json');
+  // NOTE: this is rather aggressive on avoiding caching; investigate later if needed
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Content-Length', result.length);
+
   return res
     .status(200)
-    .contentType("application/json")
-    .send(JSON.stringify(mazeData));
+    .send(result);
 });
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Route
 app.get(GENERATE_MAZE_BASE_ROUTE, (req, res) => {
   return res
     .status(200)
     .sendFile(`${__dirname}/generateMazeHelp.html`)
 });
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Server start
 app.listen(3000, () => {
+  console.log('----------------------------------------------------------------------------');
   console.log('Server running at http://localhost:3000/');
 });
